afe68207dcd4021af89848c94cb56048f35c869a
DuyBui143 <buiquangduy1403@gmail.com>
2020-11-06 21:16:46 +0700

    lan cuoi

--- demo.js
+++ demo.js
@@ -1,3 +1,7 @@
-console.log("Khởi tạo dự án");
+console.log("Khởi tạo dự án")
+// step 1 : git init
+// step 2 : add
+// step 3 : login set name and email
+console.log("code new brand")
 
-console.log("AHhihiiiiiiiiiiiiiiiiii");
+console.log("da sua Duy2");
